5661c89b0968e22154dcd7b0a1ac8fe5cb83dfeb
Ay-ob <ayoubhadjyoucef1@gmail.com>
2025-01-23 01:00:56 +0100

    update proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://project-management-1-f8ub.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
